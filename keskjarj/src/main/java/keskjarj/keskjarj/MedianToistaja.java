9c34d3a34cf9fb341b5250206e1c56ef1f8d0e63
Kahri Mikko M <mkahri@da2-lib-g115-21.helsinki.fi>
2015-05-26 14:00:01 +0300

    Pikkumuokkauksia kirjaston koneella

--- keskjarj/src/main/java/keskjarj/keskjarj/MedianToistaja.java
+++ keskjarj/src/main/java/keskjarj/keskjarj/MedianToistaja.java
@@ -19,11 +19,17 @@ public class MedianToistaja
 {
     private Path VLCPolku;
     
-    public MedianToistaja ()
-    {
-        this.VLCPolku = Paths.get("vlc"); // Toimii Linux-järj. jos VLC asennettuna
+    public MedianToistaja() {
+        if (System.getProperty("os.name").equalsIgnoreCase("linux")) {
+            this.VLCPolku = Paths.get("vlc");            
+        } else this.VLCPolku = this.findVLC();
     }
     
+    private Path findVLC()
+    {
+        return Paths.get("vlc"); 
+    }
+
     /**
      * Palauttaa VLC -mediaplayerin polun, sikäli kun sellainen on asetettu
      * @return Polku VLC -mediaplayeriin
@@ -54,35 +60,42 @@ public class MedianToistaja
     {
         if (Objects.equals(ote.getAlku(), ote.getLoppu()))
             return false;
-        
-        String komento = String.format("%s --play-and-stop %s --start-time %.3f "
+
+        String komento = String.format("%sjeejee --play-and-stop %s --start-time %.3f "
                 + "--stop-time %.3f", VLCPolku.toString(),
                 ote.getTallenne().getPolku().toString(),
                 ote.getAlku(),
                 ote.getLoppu());
-        try
+        boolean valmis = false;
+        do
         {
-            Process p = Runtime.getRuntime().exec(komento);
-        }
-        catch (IOException ex)
-        {
-            System.out.println(ex);
-            return false;
-        }
+            try {
+                Process p = Runtime.getRuntime().exec(komento);
+                valmis = true;
+            } catch (IOException ex) {
+                System.out.println(ex);
+                if (ex.getMessage().contains("error=2")) {
+                    System.out.println(this.findVLC());
+                    valmis = true;
+                }
+
+                return false;
+            }
+        } while (!valmis); 
         return true;
     }
-    
-    public static void main (String[] args)
-    {
+
+    public static void main(String[] args) {
         MedianToistaja mt = new MedianToistaja();
-        
+
         Path p = Paths.get("");
         Path pa = p.toAbsolutePath();
-        System.out.println(pa);
+        System.out.println(pa); ///home/ad/fshome4/u4/m/mkahri/Documents/Ohjelmointi/keskustelujarjestaja/keskjarj
         Path polku = Paths.get("../aineistoja/Example.mp4");
         Tallenne tallenne = new Tallenne(polku);
         Ote ote = new Ote(tallenne, 10.0, 20.0);
         mt.toista(ote);
+        System.out.println(System.getProperty("os.name").toLowerCase());
     }
 }
 
